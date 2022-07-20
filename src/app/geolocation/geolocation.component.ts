1a0f84e6d94613628d35eeda596dfd78b59d3c19
PouxvielAdrien <adri.pouxviel@hotmail.fr>
2022-07-20 15:45:51 +0200

    refactor: deleting comments

--- src/app/geolocation/geolocation.component.ts
+++ src/app/geolocation/geolocation.component.ts
@@ -1,11 +1,4 @@
-/* Import the application components and services */
-import { Component, OnInit } from '@angular/core';
-import { WeatherService, WeatherUnit } from '../_core'
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {Weather} from "../_core";
-import { finalize } from 'rxjs';
-import {HttpParams} from "@angular/common/http";
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-geolocation',
@@ -13,72 +6,5 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./geolocation.component.css']
 })
 export class GeolocationComponent {
-
   typeOfForm: string = "location";
-
-  //
-  // currentWeather: Weather | null = null;
-  // geoForm: FormGroup;
-  // locationDenied = true;
-  // isSearching = false;
-  // params: HttpParams | null = null;
-
-
-
-  constructor(private ws: WeatherService, private router:Router, private route:ActivatedRoute) {
-    // this.geoForm = new FormGroup({
-    //   geoUnit: new FormControl('metric'),
-    //   geoLang: new FormControl('en')
-    // });
-  }
-
-  //
-  // get unitFormValue():WeatherUnit{
-  //   return this.geoForm.get("geoUnit")?.value
-  // }
-  //
-  // get langFormValue():string{
-  //   return this.geoForm.get("geoLang")?.value
-  // }
-  //
-  // setGeoLocation() {
-  //   if ("geolocation" in navigator) {
-  //     this.isSearching = true;
-  //     navigator.geolocation.getCurrentPosition(
-  //       (succes) => {
-  //         this.ws.getWeatherByLocation(succes.coords.latitude,
-  //           succes.coords.longitude,
-  //            this.unitFormValue,
-  //            this.langFormValue)
-  //            .pipe(
-  //             finalize(()=> this.isSearching = false)
-  //            )
-  //           .subscribe(data => {
-  //             this.currentWeather = data;
-  //           });
-  //         },
-  //
-  //       (error) => {
-  //         this.isSearching = false;
-  //         if (error.code == error.PERMISSION_DENIED) {
-  //           this.locationDenied = false;
-  //           //TODO manage errors
-  //         }
-  //       })
-  //   }
-  //   this.changingQueryParams()
-  // }
-  //
-  // onSubmitGeoLocation() {
-  //   this.setGeoLocation();
-  // }
-  //
-  // changingQueryParams() {
-  //   this.router.navigate(
-  //     [],
-  //     {queryParams:{
-  //         unit: this.unitFormValue,
-  //         lang: this.langFormValue},
-  //       relativeTo: this.route});
-  // }
 }
